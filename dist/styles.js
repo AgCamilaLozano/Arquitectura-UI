8b93740b9cb00c00575f5882c9f02dd0fcca3e57
camila <camila.lozano@agstudios.com>
2026-04-16 11:12:53 -0500

    fix: importaciones 1.1 'css'

--- dist/styles.js
+++ dist/styles.js
@@ -1,3 +1,2 @@
-import "./chunk-2OYHW23F.js";
 import "./chunk-HXS3UQRS.js";
 import "./chunk-ORMEWXMH.js";
--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,4 @@
 'use client'
-import './styles/fonts.css'
-import './styles/globals.css'
-
 export * from './components'
 export * from './toast'
 export * from './utils'
